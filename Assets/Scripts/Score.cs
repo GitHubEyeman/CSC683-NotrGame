c169925566941ae220b99aedf14b84ba6edd6bdb
sleepyeoul <sleepygwizard123@gmail.com>
2026-01-06 21:06:31 +0800

    UI change

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
     public int score = 0;
     private int highScore;
     private int MinScore = 0;
