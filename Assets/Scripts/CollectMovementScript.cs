d4e4323ed91d98a19f3f4e912ca6a5ea65adb729
GitHubEyeman <67502641+GitHubEyeman@users.noreply.github.com>
2026-01-10 07:55:53 +0800

    Revert "Squashed commit of the following:"
    This reverts commit d2a935c6720543373cfb44e623e23ce5c810d1e9.

--- Assets/Scripts/CollectMovementScript.cs
+++ Assets/Scripts/CollectMovementScript.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
 
-public class CollectMovementScript : MonoBehaviour
+public class CollectMovement : MonoBehaviour
 {
     public float speed = 25f;
     public float destroyZ = -10f;
 
     void Update()
     {
-        // Only move if game is running
-        if (GameManager.Instance == null || !GameManager.Instance.isGameRunning || GameManager.Instance.isGamePaused)
-            return;
-            
         transform.Translate(Vector3.back * speed * Time.deltaTime);
 
         if (transform.position.z < destroyZ)
